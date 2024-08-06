3e9576bb075a695c56d85b9132bb780e817fe187
Jimyeong Kim <kjm021221@icloud.com>
2024-08-07 02:51:49 +0900

    first commit

---
+++ src/main/java/com/learning/ssltest/SsltestApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ssltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsltestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/learning/ssltest/controller/TestController.java
@@ -0,0 +1,2 @@
+package com.learning.ssltest.controller;public class TestController {
+}
